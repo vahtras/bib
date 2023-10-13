0207f26f021ccedf0dc65e61b04422d3b2c9b420
Olav Vahtras <vahtras@kth.se>
2023-10-14 01:17:22 +0200

    pytest and mongomock

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,9 @@
+import mongoengine
+import mongomock
 import pytest
 
 from models import Author, Book
+
 
 def test_create_book_with_only_title():
    book = Book(
@@ -27,3 +30,32 @@ def test_create_book_with_one_author():
         ]
     )
    assert book.title == "It's"
+
+@pytest.fixture(scope='session')
+def client():
+    yield mongoengine.connect(
+        'pytest',
+        mongo_client_class=mongomock.MongoClient,
+        alias='default'
+    )
+    mongoengine.disconnect()
+
+def test_book_to_db(mongodb, client):
+    book = Book(title='Foo')
+    book.save()
+
+    new = Book.objects().first()
+    assert new.title == 'Foo'
+
+
+def test_book_with_author_to_db(client):
+    eric = Author(first='Eric', last='Idle')
+    Book(title='Full Monty', authors=[eric]).save()
+    books = Book.objects(authors__in=[eric])
+    assert books[0].title == 'Full Monty'
+    assert books[0].authors[0].last == 'Idle'
+
+def test_titles(mongodb):
+    assert 'titles' in mongodb.list_collection_names()
+    one = mongodb.titles.find_one({'title': 'One Title'})
+    assert one['title'] == "One Title"
